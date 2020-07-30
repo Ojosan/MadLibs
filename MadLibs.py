33c46968cb77dcf8810762511bfe6389f4d5108d
Ojosan <koshka.slytherin@gmail.com>
2020-07-30 09:59:30 -0400

    Mad Libs in Python

---
+++ MadLibs.py
@@ -0,0 +1,26 @@
+
+holiday = "christmas"
+
+noun = "hogwarts"
+
+place = "harry potter world"
+
+nounTwo = "harry potter"
+
+adjective = "red"
+
+pluralBpart = "hands"
+
+verb = "running"
+
+adj = "big"
+
+nounThree = "pants"
+
+nounFour = "shirt"
+
+plurNoun = "fingers"
+
+
+
+print (f" I can't believe it's already {holiday}. I cant wait to put on my {noun} and visit every {place} around my block. This year im gonna dress up as (a) {nounTwo} with {adjective} {pluralBpart}. before i {verb} i make sure to grab my {adj} {nounThree} to hold all my {nounFour}. Finally all my {plurNoun} are ready to go.")
